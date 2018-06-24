2e7caf5f799794f1223225c386fac37f107b84bf
Ali AVCI <aliavci82@gmail.com>
2018-06-24 19:17:31 +0300

    Folder Structure Change

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import Scroll from './Scroll';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
 
 class App extends Component {
 	state = {
@@ -20,18 +20,15 @@ class App extends Component {
 	}
 
 	render() {
-		const filteredRobots = this.state.robots
+		const { robots, searchField} = this.state;
+		const filteredRobots = robots
 			.filter(robot => 
 					{ 
-						return robot.name.toLowerCase().includes(this.state.searchField) 
+						return robot.name.toLowerCase().includes(searchField) 
 					});
 
-		if(this.state.robots.length === 0) 
-		{
-			return <h1 className="tc">Loading</h1>;
-		} 
-		else {
-				return (
+		return (!robots.length) ? <h1 className="tc">Loading</h1> : 
+					(
 					<div className="tc">
 						<h1>RoboFriends</h1>
 						<SearchBox onSearchChange={this.onSearchChange} />
@@ -40,8 +37,8 @@ class App extends Component {
 						</Scroll>
 						
 					</div>
-		);
-		}
+					);
+		
 	
 	}
 }
